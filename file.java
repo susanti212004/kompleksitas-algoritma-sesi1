1392a45600e7253e120280bcaffded9424375f53
susanti212004 <123722597+susanti212004@users.noreply.github.com>
2023-09-27 14:45:19 +0700

    Add files via upload

---
+++ file.java
@@ -0,0 +1,10 @@
+public class file {
+    public static void main(String[] args){
+        int nilai =2;
+        for (; nilai<=2;){
+            System.out.println("j = " + nilai);
+            nilai++;
+        }
+    }
+    
+}
